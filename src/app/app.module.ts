f3380d66815481bd4cdf6f7a15a5d7191d854c85
Matias10400F <ma.aguayom@duocuc.cl>
2023-07-18 21:41:43 -0400

    Repasando Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    //Cuando creamos un nuevo componente se tiene que declarar aca
+    AppComponent,
+
+
+
+  ],//Aca van los modulos
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]

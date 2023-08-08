f29dd6ed9529297b51b0841d600976a49f6d800f
Matias10400F <ma.aguayom@duocuc.cl>
2023-08-07 23:47:39 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
   declarations: [
-    //Cuando creamos un nuevo componente se tiene que declarar aca
+    //Cuando creamos un nuevo componente se tiene que declarar aca los .components
     AppComponent,
 
 
@@ -18,6 +20,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule
 
   ],
   providers: [],
